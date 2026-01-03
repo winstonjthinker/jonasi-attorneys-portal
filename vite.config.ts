5facbac91860149b3cd5ffab4e0679f63ebf1445
winstonjthinker <winstonjthinkersavens@gmail.com>
2026-01-03 18:29:21 +0200

    the rightful changes no sweat

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+ 
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
